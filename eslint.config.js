97ea5d6e66b06b94fcdaac0cfd75506f36b682e2
Ruben Shalimov <r_shalimov@inbox.ru>
2025-11-08 10:43:42 +0300

    `StyleSheet` and `Text` are ignored by `@typescript-eslint/no-shadow`.

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,18 @@
+import { ts } from '@robinbobin/eslint-config/ruleOptions'
 import parentConfig from '@robinbobin/eslint-config-react'
 import { defineConfig } from 'eslint/config'
 
-export default defineConfig(parentConfig)
+export default defineConfig([
+  ...parentConfig,
+  {
+    rules: {
+      '@typescript-eslint/no-shadow': [
+        'error',
+        {
+          ...ts.noShadow,
+          allow: ['StyleSheet', 'Text']
+        }
+      ]
+    }
+  }
+])
